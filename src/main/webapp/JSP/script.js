d61a8b1b57078dccc021f816831f6100bfe58092
Oshadha <oshadhathinurakithmina@gmail.com>
2025-05-06 09:03:41 +0530

    local host button navigation change2

--- src/main/webapp/JSP/script.js
+++ src/main/webapp/JSP/script.js
@@ -30,5 +30,5 @@ document.getElementById("registerForm").addEventListener("submit", function(even
     localStorage.setItem("users", JSON.stringify(users));
 
     alert("Registration successful! You can now log in.");
-    window.location.href = "register.jsp"; // Redirect to login page
+    window.location.href = "index.jsp"; // Redirect to login page
 });
